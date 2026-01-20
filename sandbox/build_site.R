cfeaec606b4070f5c56b5f5b6e790e538d25f71d
aroles <aroles@oberlin.edu>
2026-01-20 13:51:04 -0500

    image fix
    trying to fix things so i can correctly reference images

--- sandbox/build_site.R
+++ sandbox/build_site.R
@@ -5,9 +5,16 @@
 # run above if updated R otherwise package will not be found
 # also install any packages needed
 
+# This clears out the old website and rebuilds every article from scratch
+# pkgdown::clean_site()
+# pkgdown::build_site(devel = FALSE)
+
+# run below to rebuild the site
 pkgdown::build_site_github_pages()
 
 base_sheets <- file.path(here::here(), 'sheets')
 articles_sheets <- file.path(here::here(), 'docs', 'articles', 'sheets')
 
 fs::dir_copy(base_sheets, articles_sheets)
+
+
